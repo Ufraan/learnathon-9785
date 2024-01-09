2d1baf11063f84a1ec857eab318a72925a3e80ee
Ufraan <ufraansiddiqui30@gmail.com>
2024-01-09 20:50:03 +0530

    20:50

---
+++ day2/hello.py
@@ -0,0 +1,4 @@
+name = input('Please enter your name: ')
+age = input('Please enter your age: ')
+Department = input
+print('Hello ' + name + ' you are ' + age + ' years old and you are in ' + Department + ' department')
