ebf13a9a3ffa399126a189ceba7f4f9a8dada005
Ufraan <ufraansiddiqui30@gmail.com>
2024-01-27 21:08:06 +0530

    adding elements to a list using insert() method

---
+++ day_10/lists/add_elements_to_list/insert.py
@@ -0,0 +1,18 @@
+#insert method adds an element at a specified position in the list
+#for example:
+
+numbers = [10, 30, 40]
+
+# insert an element at index 1 (second position)
+numbers.insert(1, 20)
+
+print(numbers) # [10, 20, 30, 40]
+
+
+#in this, the element 20 is added at index 1 (second position) and the output is [10, 20, 30, 40]
+
+#so output will be as follows:
+#[10, 20, 30, 40]
+
+
+# Path: day_10/lists/append.py
