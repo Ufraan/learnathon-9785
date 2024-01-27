8dcf6569bf7d0422204705ff825e8c578eb857f7
Ufraan <ufraansiddiqui30@gmail.com>
2024-01-27 21:09:27 +0530

    change list items

---
+++ day_10/lists/change_list_items.py
@@ -0,0 +1,13 @@
+#items in a list can be changed and are mutable. we use the assignment operator (=) to change an item or a range of items
+
+languages = ['Python', 'Swift', 'C++']
+
+# changing the third item to 'C'
+languages[2] = 'C'
+
+print(languages)  # ['Python', 'Swift', 'C']
+
+#here, initially the third item was C++ but we changed it to C using the assignment operator (=)
+
+#so output is will be as follows:
+# ['Python', 'Swift', 'C']
