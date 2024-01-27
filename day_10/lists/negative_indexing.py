f8121d041f2f420a1c1e955272038d0905e55a38
Ufraan <ufraansiddiqui30@gmail.com>
2024-01-27 21:03:38 +0530

    negative indexing in python

---
+++ day_10/lists/negative_indexing.py
@@ -0,0 +1,13 @@
+#negative indexing in lists means that the last element has index -1, the second last has index -2 and so on...
+
+languages = ["Python", "Swift", "C++"]
+
+# access item at index 0
+print(languages[-1])   # C++
+
+# access item at index 2
+print(languages[-3])   # Python
+
+#in this, the last element has index -1, the second last has index -2 and so on... so the output is C++ and Python and not Python and C++..
+
+# Path: day_10/lists/append.py
