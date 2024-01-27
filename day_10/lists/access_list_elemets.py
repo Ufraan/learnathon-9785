0f19f3aa63c49b3b7ce0bfdf14ee74f90cc9dc92
Ufraan <ufraansiddiqui30@gmail.com>
2024-01-27 21:02:29 +0530

    list index

---
+++ day_10/lists/access_list_elemets.py
@@ -0,0 +1,15 @@
+#in python lists are ordered and each element has an index,
+#the first element has index 0, the second has index 1 and so on...
+
+
+languages = ["Python", "Swift", "C++"]
+
+# access item at index 0
+print(languages[0])   # Python
+
+# access item at index 2
+print(languages[2])   # C++
+
+
+
+
