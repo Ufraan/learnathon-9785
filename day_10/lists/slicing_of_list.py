50698e869709a497df45a2fc0444eb5860441bbb
Ufraan <ufraansiddiqui30@gmail.com>
2024-01-27 21:04:50 +0530

    slicing of list in python

---
+++ day_10/lists/slicing_of_list.py
@@ -0,0 +1,15 @@
+#list slicing in python means that we can access a range of elements in a list by using the slicing operator colon(:)
+#syntax: list_name[start:end]
+#this is used to access a range of elements in a list and not just one element like i had done in day_10/lists/access_list_elemets.py
+
+
+my_list = ['p','r','o','g','r','a','m','i','z']
+
+# items from index 2 to index 4
+print(my_list[2:5])
+
+# items from index 5 to end
+print(my_list[5:])
+
+# items beginning to end
+print(my_list[:])
