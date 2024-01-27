4ac275362e009499a2161b4135319d0fbd02349f
Ufraan <ufraansiddiqui30@gmail.com>
2024-01-27 21:06:10 +0530

    adding elements to list -- ammend

---
+++ day_10/lists/add_elements_to_list/append.py
@@ -0,0 +1,16 @@
+#the append method adds an element to the end of the list
+numbers = [21, 34, 54, 12]
+
+print("Before Append:", numbers)
+
+# using append method
+numbers.append(32)
+
+print("After Append:", numbers)
+
+#output will be as follows:
+# Before Append: [21, 34, 54, 12]
+# After Append: [21, 34, 54, 12, 32]
+
+
+# Path: day_10/lists/append.py
